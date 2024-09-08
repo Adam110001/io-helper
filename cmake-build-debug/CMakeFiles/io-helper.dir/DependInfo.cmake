9453b7874225f238fe88312d6aee448458f37c9e
Dovciak, Adam <a.dovciak@liverpool.ac.uk>
2024-09-08 10:10:44 +0100

    Updated CMakeRepo

---
+++ cmake-build-debug/CMakeFiles/io-helper.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/adamdovciak/CLionProjects/io-helper/GeneralHelpers/Options.c" "CMakeFiles/io-helper.dir/GeneralHelpers/Options.c.o" "gcc" "CMakeFiles/io-helper.dir/GeneralHelpers/Options.c.o.d"
+  "/Users/adamdovciak/CLionProjects/io-helper/GeneralHelpers/SignalHandlers.c" "CMakeFiles/io-helper.dir/GeneralHelpers/SignalHandlers.c.o" "gcc" "CMakeFiles/io-helper.dir/GeneralHelpers/SignalHandlers.c.o.d"
+  "/Users/adamdovciak/CLionProjects/io-helper/USB/USBHandler.c" "CMakeFiles/io-helper.dir/USB/USBHandler.c.o" "gcc" "CMakeFiles/io-helper.dir/USB/USBHandler.c.o.d"
+  "/Users/adamdovciak/CLionProjects/io-helper/main.c" "CMakeFiles/io-helper.dir/main.c.o" "gcc" "CMakeFiles/io-helper.dir/main.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/io-helper.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/io-helper.dir/GeneralHelpers/Options.c.o"
+  "CMakeFiles/io-helper.dir/GeneralHelpers/Options.c.o.d"
+  "CMakeFiles/io-helper.dir/GeneralHelpers/SignalHandlers.c.o"
+  "CMakeFiles/io-helper.dir/GeneralHelpers/SignalHandlers.c.o.d"
+  "CMakeFiles/io-helper.dir/USB/USBHandler.c.o"
+  "CMakeFiles/io-helper.dir/USB/USBHandler.c.o.d"
+  "CMakeFiles/io-helper.dir/main.c.o"
+  "CMakeFiles/io-helper.dir/main.c.o.d"
+  "io-helper"
+  "io-helper.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/io-helper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
